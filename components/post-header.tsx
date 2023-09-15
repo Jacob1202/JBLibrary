ac0cf7e0cd0a372f209f6ab8515fb64a1ba5ffc2
Jakub Biniek <kuba.biniek01@gmail.com>
2023-09-15 14:56:53 +0200

    fix deploy

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,6 +1,5 @@
 import DateFormatter from "./DateFormatter";
 import type Author from "../interfaces/author";
-import Avatar from "./Avatar";
 
 type Props = {
   title: string;
@@ -13,14 +12,11 @@ const PostHeader = ({ date, author }: Props) => {
   return (
     <>
       <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
         <DateFormatter dateString={date} />
       </div>
 
       <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
+        <div className="block md:hidden mb-6"></div>
       </div>
     </>
   );
