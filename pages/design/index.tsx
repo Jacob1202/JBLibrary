d1289f40545eb082348ece19303125125317c1a1
Jakub Biniek <kuba.biniek01@gmail.com>
2023-09-17 19:43:45 +0200

    fix

--- pages/design/index.tsx
+++ pages/design/index.tsx
@@ -8,7 +8,7 @@ export default function PageDOTNET({ posts }: Props) {
   return (
     <div className="sm:px-0 px-4 flex flex-col mt-10 container mx-auto min-h-screen">
       <h1 className="text-2xl md:text-3xl lg:text-5xl font-semibold md:leading-none my-4">
-        Design showcase
+        Design showcase - Coming soon
       </h1>
       <div></div>
     </div>
